cfe2c318ecb0cd33009499bcfcdd2f8e83c73bbe
SSJSerg <46301252+SSJSerg@users.noreply.github.com>
2020-03-11 00:13:59 -0700

    Google Map API/Input Fields/Background Color Added
    Signed-off-by: SSJSerg <46301252+SSJSerg@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SearchInputFormComponent } from './search-input-form/search-input-form.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatFormFieldModule
+  ],
+  exports: [
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search-input-form/search-input-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input-form',
+  templateUrl: './search-input-form.component.html',
+  styleUrls: ['./search-input-form.component.css']
+})
+export class SearchInputFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
